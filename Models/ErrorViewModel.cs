5a1a239f42ddad4fef6a7e91073497f29d18f570
Anthaguz <anthonyag7@hotmail.com>
2024-03-03 16:13:41 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Case_estudio1_Grupo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
