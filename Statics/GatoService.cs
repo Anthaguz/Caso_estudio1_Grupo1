4ba6988d5406142599e6a28f2dcff4c471d46f95
CCisnerosG <cabeto19851@gmail.com>
2024-03-06 22:28:35 -0600

    stage

--- Statics/GatoService.cs
+++ Statics/GatoService.cs
@@ -20,16 +20,89 @@
 
 		public static int JugadaComputadora(string[][] valores)
 		{
-			//valor random de la computadora
 			Random rnd = new Random();
-			//Se busca un espacio vacio. Es vacio si el valor no es ni X ni O
-
 			int jugada = rnd.Next(0, 9);
+
 			while (valores[jugada][1] == "X" || valores[jugada][1] == "O")
 			{
 				jugada = rnd.Next(0, 9);
 			}
+
 			return jugada;
+		}
+
+        public static string VerificarEstadoJuego(string[][] valores)
+        {
+            string[,] tablero = ConvertirAArrayBidimensional(valores);
+
+            if (HayGanador(tablero, "X"))
+            {
+                return "X"; // La computadora (X) ha ganado
+            }
+            else if (HayGanador(tablero, "O"))
+            {
+                return "O"; // El jugador (O) ha ganado
+            }
+            else if (EsEmpate(tablero))
+            {
+                return "Empate"; // El juego ha terminado en empate
+            }
+            else
+            {
+                return "EnCurso"; // El juego está en curso
+            }
+        }
+
+        private static bool HayGanador(string[,] tablero, string jugador)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tablero[i, 0] == jugador && tablero[i, 1] == jugador && tablero[i, 2] == jugador)
+                    return true; // Ganador en una fila
+
+                if (tablero[0, i] == jugador && tablero[1, i] == jugador && tablero[2, i] == jugador)
+                    return true; // Ganador en una columna
+            }
+
+            if (tablero[0, 0] == jugador && tablero[1, 1] == jugador && tablero[2, 2] == jugador)
+                return true; // Ganador en la diagonal principal
+
+            if (tablero[0, 2] == jugador && tablero[1, 1] == jugador && tablero[2, 0] == jugador)
+                return true; // Ganador en la diagonal secundaria
+
+            return false;
+        }
+
+        private static bool EsEmpate(string[,] tablero)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tablero[i, j] == null)
+                        return false; // Todavía hay al menos una casilla vacía
+                }
+            }
+
+            return true; // Todas las casillas están ocupadas y no hay ganador
+        }
+
+		private static string[,] ConvertirAArrayBidimensional(string[][] valores)
+		{
+			string[,] tablero = new string[3, 3];
+
+			for (int i = 0; i < valores.Length; i++)
+			{
+				if (int.TryParse(valores[i][0].Substring(1), out int casilla))
+				{
+					casilla--; // Restar 1 si la conversión fue exitosa
+					int fila = casilla / 3;
+					int columna = casilla % 3;
+					tablero[fila, columna] = valores[i][1];
+				}
+			}
+
+			return tablero;
 		}
 	}
 }
