356353fdd13d07a64bdd96e33e8a78844debfc86
Moises26b <mbenavides10786@ufide.ac.cr>
2024-03-09 14:34:42 -0600

    Gato imposible

--- Statics/GatoService.cs
+++ Statics/GatoService.cs
@@ -1,36 +1,96 @@
 ﻿namespace Caso_estudio1_Grupo1.Statics
 {
-	public static class GatoService
-	{
-		public static string GenerarRespuesta(string[][] valores)
-		{
-			string respuesta =
-					valores[0][0] + "=" + valores[0][1] + "&" +
-					valores[1][0] + "=" + valores[1][1] + "&" +
-					valores[2][0] + "=" + valores[2][1] + "&" +
-					valores[3][0] + "=" + valores[3][1] + "&" +
-					valores[4][0] + "=" + valores[4][1] + "&" +
-					valores[5][0] + "=" + valores[5][1] + "&" +
-					valores[6][0] + "=" + valores[6][1] + "&" +
-					valores[7][0] + "=" + valores[7][1] + "&" +
-					valores[8][0] + "=" + valores[8][1] + "&" +
-					valores[9][0] + "=" + valores[9][1];
-			return respuesta;
-		}
+    public static class GatoService
+    {
+        public static string GenerarRespuesta(string[][] valores)
+        {
+            string respuesta =
+                    valores[0][0] + "=" + valores[0][1] + "&" +
+                    valores[1][0] + "=" + valores[1][1] + "&" +
+                    valores[2][0] + "=" + valores[2][1] + "&" +
+                    valores[3][0] + "=" + valores[3][1] + "&" +
+                    valores[4][0] + "=" + valores[4][1] + "&" +
+                    valores[5][0] + "=" + valores[5][1] + "&" +
+                    valores[6][0] + "=" + valores[6][1] + "&" +
+                    valores[7][0] + "=" + valores[7][1] + "&" +
+                    valores[8][0] + "=" + valores[8][1] + "&" +
+                    valores[9][0] + "=" + valores[9][1];
+            return respuesta;
+        }
+        private static string[][] ClonarTablero(string[][] tablero)
+        {
+            string[][] nuevoTablero = new string[tablero.Length][];
+            for (int i = 0; i < tablero.Length; i++)
+            {
+                nuevoTablero[i] = new string[2];
+                nuevoTablero[i][0] = tablero[i][0];
+                nuevoTablero[i][1] = tablero[i][1];
+            }
+            return nuevoTablero;
+        }
+        public static int JugadaComputadora(string[][] valores)
+        {
+            int mejorValor = int.MinValue;
+            int mejorJugada = -1;
 
-		public static int JugadaComputadora(string[][] valores)
-		{
-			Random rnd = new Random();
-			int jugada = rnd.Next(0, 9);
+            for (int i = 0; i < valores.Length; i++)
+            {
+                if (valores[i][1] != "X" && valores[i][1] != "O")
+                {
+                    string[][] nuevoTablero = ClonarTablero(valores);
+                    nuevoTablero[i][1] = "X"; // La computadora juega como "X"
+                    int valor = Minimax(nuevoTablero, false);
+                    if (valor > mejorValor)
+                    {
+                        mejorValor = valor;
+                        mejorJugada = i;
+                    }
+                }
+            }
 
-			while (valores[jugada][1] == "X" || valores[jugada][1] == "O")
-			{
-				jugada = rnd.Next(0, 9);
-			}
+            return mejorJugada;
+        }
 
-			return jugada;
-		}
+        private static int Minimax(string[][] tablero, bool esMaximizando)
+        {
+            string estadoJuego = VerificarEstadoJuego(tablero);
 
+            if (estadoJuego == "X")
+                return 10; // La computadora gana
+            else if (estadoJuego == "O")
+                return -10; // El jugador gana
+            else if (estadoJuego == "Empate")
+                return 0; // Empate
+
+            if (esMaximizando)
+            {
+                int mejorValor = int.MinValue;
+                for (int i = 0; i < tablero.Length; i++)
+                {
+                    if (tablero[i][1] != "X" && tablero[i][1] != "O")
+                    {
+                        string[][] nuevoTablero = ClonarTablero(tablero);
+                        nuevoTablero[i][1] = "X"; // La computadora juega como "X"
+                        mejorValor = Math.Max(mejorValor, Minimax(nuevoTablero, false));
+                    }
+                }
+                return mejorValor;
+            }
+            else
+            {
+                int mejorValor = int.MaxValue;
+                for (int i = 0; i < tablero.Length; i++)
+                {
+                    if (tablero[i][1] != "X" && tablero[i][1] != "O")
+                    {
+                        string[][] nuevoTablero = ClonarTablero(tablero);
+                        nuevoTablero[i][1] = "O"; // El jugador juega como "O"
+                        mejorValor = Math.Min(mejorValor, Minimax(nuevoTablero, true));
+                    }
+                }
+                return mejorValor;
+            }
+        }
         public static string VerificarEstadoJuego(string[][] valores)
         {
             string[,] tablero = ConvertirAArrayBidimensional(valores);
@@ -87,22 +147,22 @@
             return true; // Todas las casillas están ocupadas y no hay ganador
         }
 
-		private static string[,] ConvertirAArrayBidimensional(string[][] valores)
-		{
-			string[,] tablero = new string[3, 3];
+        private static string[,] ConvertirAArrayBidimensional(string[][] valores)
+        {
+            string[,] tablero = new string[3, 3];
 
-			for (int i = 0; i < valores.Length; i++)
-			{
-				if (int.TryParse(valores[i][0].Substring(1), out int casilla))
-				{
-					casilla--; // Restar 1 si la conversión fue exitosa
-					int fila = casilla / 3;
-					int columna = casilla % 3;
-					tablero[fila, columna] = valores[i][1];
-				}
-			}
+            for (int i = 0; i < valores.Length; i++)
+            {
+                if (int.TryParse(valores[i][0].Substring(1), out int casilla))
+                {
+                    casilla--; // Restar 1 si la conversión fue exitosa
+                    int fila = casilla / 3;
+                    int columna = casilla % 3;
+                    tablero[fila, columna] = valores[i][1];
+                }
+            }
 
-			return tablero;
-		}
-	}
+            return tablero;
+        }
+    }
 }
